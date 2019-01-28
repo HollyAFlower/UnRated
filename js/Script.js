63eab8b4faeffc0a00d534d5fb1215f82468148a
hyper2snyper <wytheswagger@gmail.com>
2019-01-28 12:53:32 -0800

    My site is up!

---
+++ js/Script.js
@@ -0,0 +1,5 @@
+var int1 = prompt("please enter a number", "1");
+var int2 = promt("please enter a second number", "10");
+var = i;
+for (int1 < int2; i++)
+document.getElementById(p).innerHTML = i;
