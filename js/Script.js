6956e6d0332ff354168f7157707cf5741787b13e
hyper2snyper <wytheswagger@gmail.com>
2019-01-28 13:17:32 -0800

    #2

--- js/Script.js
+++ js/Script.js
@@ -1,5 +1,5 @@
-var int1 = prompt("please enter a number", "1");
-var int2 = promt("please enter a second number", "10");
-var = i;
-for (int1 < int2; i++)
-document.getElementById(p).innerHTML = i;
+var person = prompt("Please enter your name", "Harry Potter");
+if (person != null) {
+  document.getElementById("Script").innerHTML =
+  "Hello " + person + "! How are you today?";
+}
