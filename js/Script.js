e643957f15cad6272fe9719bb715a911ae5f58e7
hyper2snyper <wytheswagger@gmail.com>
2019-01-30 12:44:33 -0800

    #6

--- js/Script.js
+++ js/Script.js
@@ -1,10 +1,1 @@
 var i = 0;
-var T = true;
-function UpDoot() {
-  i++
-};
-for (i == true) {
-  document.getElementById("Print").innerHTML =
-  i;
-};
-document.getElementById(Print).innerHTML = 5 + 6;
