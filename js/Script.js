d2395cb87cb8f79303f38e6ab1a81dbd29a44c57
hyper2snyper <wytheswagger@gmail.com>
2019-01-30 18:04:20 -0800

    #7

--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,8 @@
-document.getElementById("demo").innerHTML = 5 + 6;
+function MyFunction() {
+  var Name = prompt("Please enter your name.", "Type name here.");
+  if (Name != "Type name here.") {
+    document.getElementById("demo").innerHTML = "Hello " + Name + ".";
+  } else {
+    document.getElementById("demo").innerHTML = "You didn't type anything silly.";
+  }
+}
