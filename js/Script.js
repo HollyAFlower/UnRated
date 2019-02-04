c9af143c2159ae695a205cac60602a2aecaab135
hyper2snyper <wytheswagger@gmail.com>
2019-02-04 13:51:22 -0800

    12

--- js/Script.js
+++ js/Script.js
@@ -1,14 +1,17 @@
 function MyFunction() {
-  var Name = prompt("Please enter your name.", "0");
-  if (Name != "Type name here.") {
-    var Age = Number(prompt("Type your age.", "Age goes here."));
+  var Name = prompt("Please enter your name.", "Name goes here.");
+  if (Name != "Names goes here.") {
+    var Age = Number(prompt("Type your age.", "0"));
     document.getElementById("demo").innerHTML = "Hello " + Name + ".";
     if (Age != "0") {
         document.getElementById("demo").innerHTML = "Hello " + Name + ". You are " + Age + " years old.";
     } else {
-        document.getElementById("demo").innerHTML = "Hello " + Name + "." + "But of course you cant be 0 years old.";
+        document.getElementById("demo").innerHTML = "Hello " + Name + ". " + "But of course you cant be 0 years old.";
     }
   } else {
     document.getElementById("demo").innerHTML = "You didn't type anything silly.";
   }
+}
+function MyFunction2() {
+    
 }
