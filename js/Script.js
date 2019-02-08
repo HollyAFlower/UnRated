bd2e12ddd6161c480858dc251cfa02b1dbee6cf2
hyper2snyper <wytheswagger@gmail.com>
2019-02-08 13:33:53 -0800

    4567

--- js/Script.js
+++ js/Script.js
@@ -14,9 +14,33 @@ function MyFunction() {
 }
 function Add() {
   var I1 = Number(prompt("Type a number.", 0));
-  var I2 = Number(prompt("Tupe a second number.", 1));
+  var I2 = Number(prompt("Type a second number.", 1));
   var I3 = I1 + I2;
   if (true) {
     document.getElementById("demo2").innerHTML = "The sum of " + I1 + " and " + I2 + " is " + I3 + ".";
+  }
+}
+function Multiply() {
+  var I1 = Number(prompt("Type a number.", 0));
+  var I2 = Number(prompt("Type a second number.", 1));
+  var I3 = I1 * I2;
+  if (true) {
+    document.getElementById("demo2").innerHTML = "The multiple of " + I1 + " and " + I2 + " is " + I3 + ".";
+  }
+}
+function Devide() {
+  var I1 = Number(prompt("Type a number.", 0));
+  var I2 = Number(prompt("Type a second number.", 1));
+  var I3 = I1 / I2;
+  if (true) {
+    document.getElementById("demo2").innerHTML = "The devisor of " + I1 + " and " + I2 + " is " + I3 + ".";
+  }
+}
+function Subtract() {
+  var I1 = Number(prompt("Type a number.", 0));
+  var I2 = Number(prompt("Type a second number.", 1));
+  var I3 = I1 - I2;
+  if (true) {
+    document.getElementById("demo2").innerHTML = "The difference of " + I1 + " and " + I2 + " is " + I3 + ".";
   }
 }
